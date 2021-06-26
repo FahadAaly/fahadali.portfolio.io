9b0395ebc651277094dbf3fdf7aeb4ae9552f7cd
FahadAaly <ali-fahad1234@hotmail.com>
2021-06-27 01:13:42 +0500

    First comment

---
+++ src/components/about.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-12">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effect="fadeInLeft"
+                >
+                  <div className="col-md-12">
+                    <div className="about-desc">
+                      <span className="heading-meta">About Me</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>
+                        I'm a full-stack developer specialised in frontend and
+                        backend development for complex scalable web apps.
+                      </p>
+                      <p>
+                        Has over 3 years of working experience in the software
+                        development field, building projects for the web and
+                        mobile platforms, using different programming tools like
+                        JavaScript, TypeScript, PHP, HTML & CSS. I love
+                        exploring new technologies and mostly focused on
+                        frontend technologies.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
---
+++ src/components/experience.jsx
@@ -0,0 +1,154 @@
+import React, { Component } from "react";
+
+export default class Experience extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="experience">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">EXPERIENCE</span>
+                <h2 className="colorlib-heading animate-box">
+                  WORK EXPERIENCE
+                </h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Senior Full Stack developer at Progstream{" "}
+                          <span>2021-present</span>
+                        </h2>
+                        <p>
+                          Document and follow best design and development
+                          practices.
+                        </p>
+                        <p>
+                          Participate in design and code reviews to ensure best
+                          practices and high quality code.
+                        </p>
+                        <p>
+                          Experience with ReactJs, MERN stack and other
+                          frameworks.
+                        </p>
+                        <p>
+                          Solid understanding of web technologies such as REST,
+                          HTTP, JSON.
+                        </p>
+                        <p>
+                          Experience using Bootstrap or similar responsive
+                          framework.
+                        </p>
+                        <p>
+                          Familiar with development tools such as
+                          cross-compilation, source revision control (GIT), bug
+                          tracking (JIRA).
+                        </p>
+                        <p>
+                          Ensure quality of technology delivery of multiple
+                          vendors’ technology solutions for multiple concurrent
+                          projects or programs for an application family.
+                        </p>
+                        <p>Lead the team and help solve problems.</p>
+                        <p>Excited about mentoring junior developers.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Full Stack developer at ProgStream{" "}
+                          <span>2019-2021</span>
+                        </h2>
+                        <p>
+                          Work with development teams and product managers to
+                          ideate software solutions.
+                        </p>
+                        <p>Build the front-end of applications.</p>
+                        <p>
+                          Develop and manage well-functioning databases and
+                          applications.
+                        </p>
+                        <p>Write effective APIs.</p>
+                        <p>
+                          Build features and applications with a mobile
+                          responsive design.
+                        </p>
+                        <p>
+                          Test applications to ensure responsiveness and
+                          efficiency by doing automation.
+                        </p>
+                        <p>
+                          Gained hands-on expertise in Javascript, Typescript
+                          and frontend frameworks React, Angular, Next.js as
+                          well as backend Node.js, PHP, Express.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Frontend developer at AppBakerz <span>2018-2019</span>
+                        </h2>
+                        <p>
+                          Cooperate with designers to create clean interfaces
+                          and simple, intuitive interactions and experiences.
+                        </p>
+                        <p>
+                          Designs, develops web applications of clients using
+                          the language tools HTML, CSS, Javascript, Typescript
+                          and frameworks Angular, React, Vue.js.
+                        </p>
+                        <p>
+                          Develop project concepts and maintain optimal
+                          workflow.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
